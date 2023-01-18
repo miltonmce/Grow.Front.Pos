144ba1a3b1e4a268827b4b089aff5a1ea5c479d0
Milton Medina C <miltonmce@gmail.com>
2023-01-17 19:51:50 -0700

    basic structure

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,21 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/navbar/NavBar.vue';
+import SideBar from './components/sidebar/SideBar.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+<div class="container-fluid">
+  <div class="row">
+    <NavBar></NavBar>
+    <SideBar></SideBar>
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+      <router-view></router-view>
+    </main>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+</div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,30 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+import axios from 'axios';
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import router from './router';
+import store from './store';
+import "bootstrap/dist/css/bootstrap.css";
+
+const app = createApp(App);
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://localhost:8000/';  // the FastAPI backend
+
+axios.interceptors.response.use(undefined, function (error) {
+    if (error) {
+      const originalRequest = error.config;
+      if (error.response.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true;
+        store.dispatch('logOut');
+        return router.push('/login')
+      }
+    }
+  });
+
+app.use(router);
+app.use(store); 
+app.mount("#app"); 
+
+import "bootstrap/dist/js/bootstrap.js";
